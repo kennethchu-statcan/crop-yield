624052f3db19d2f7b58aed0b0c7c9bd8aab7c924
paradisepilot <paradisepilot@gmail.com>
2020-08-17 17:27:35 -0400

    updated: run-main-build-package.sh (added --as-cran to 'R CMD check' command)

--- 101-package-stcCropYield/run-main-build-package.sh
+++ 101-package-stcCropYield/run-main-build-package.sh
@@ -14,7 +14,10 @@ cp    $0         ${outputDIR}/code
 ########################################################
 ##### Assemble contents of R package
 packageName=stcCropYield
-Rscript ${codeDIR}/main-build-package.R ${codeDIR} ${outputDIR} ${packageName} > ${outputDIR}/stdout.R.main-build-package 2> ${outputDIR}/stderr.R.main-build-package
+myRscript=${codeDIR}/main-build-package.R
+stdoutFile=${outputDIR}/stdout.R.`basename ${myRscript} .R`
+stderrFile=${outputDIR}/stderr.R.`basename ${myRscript} .R`
+Rscript ${codeDIR}/main-build-package.R ${codeDIR} ${outputDIR} ${packageName} > ${stdoutFile} 2> ${stderrFile}
 
 ##### Build the R package (create *.tar.gz file)
 cd ${outputDIR}
@@ -24,5 +27,5 @@ R CMD build --md5 --no-build-vignettes ${outputDIR}/${packageName} > ${outputDIR
 # remember: define the environment variable R_LIBS_USER
 # so this build script knows where the dependencies are,
 # so that the following command can work properly.
-R CMD check ${packageName}_*.tar.gz > ${outputDIR}/stdout.R.check 2> ${outputDIR}/stderr.R.check
+R CMD check --as-cran ${packageName}_*.tar.gz > ${outputDIR}/stdout.R.check 2> ${outputDIR}/stderr.R.check
 
