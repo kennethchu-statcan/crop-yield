97edf82b79cad0293ddb65e54110607ef66b7b97
paradisepilot <paradisepilot@gmail.com>
2020-08-17 00:14:49 -0400

    updated: initializePlot.R

--- 901-dev/code/initializePlot.R
+++ 901-dev/code/initializePlot.R
@@ -4,24 +4,24 @@ initializePlot <- function(
     textsize.axis  = 20
     ) {
 
-    require(ggplot2);
+    #base::require(ggplot2);
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-    output.ggplot <- ggplot(data = NULL) + theme_bw();
-    output.ggplot <- output.ggplot + theme(
-        title            = element_text(size = textsize.title, face = "bold"),
-        legend.text      = element_text(size = textsize.axis,  face = "bold"),
-        axis.text.x      = element_text(size = textsize.axis,  face = "bold"),
-        axis.text.y      = element_text(size = textsize.axis,  face = "bold"),
-        #axis.title.x     = element_blank(),
-        #axis.title.y     = element_blank(),
-        panel.grid.major = element_line(colour="gray", linetype=2, size=0.25),
-        panel.grid.minor = element_line(colour="gray", linetype=2, size=0.25),
+    output.ggplot <- ggplot2::ggplot(data = NULL) + ggplot2::theme_bw();
+    output.ggplot <- output.ggplot + ggplot2::theme(
+        title            = ggplot2::element_text(size = textsize.title, face = "bold"),
+        legend.text      = ggplot2::element_text(size = textsize.axis,  face = "bold"),
+        axis.text.x      = ggplot2::element_text(size = textsize.axis,  face = "bold"),
+        axis.text.y      = ggplot2::element_text(size = textsize.axis,  face = "bold"),
+        #axis.title.x    = ggplot2::element_blank(),
+        #axis.title.y    = ggplot2::element_blank(),
+        panel.grid.major = ggplot2::element_line(colour="gray", linetype=2, size=0.25),
+        panel.grid.minor = ggplot2::element_line(colour="gray", linetype=2, size=0.25),
         legend.position  = "bottom",
         legend.key.width = ggplot2::unit(0.75,"in")
         );
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-    return( output.ggplot );
+    base::return( output.ggplot );
 
     }
