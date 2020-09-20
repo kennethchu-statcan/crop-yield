f92e9f3bde163cd2c8e2faf8757c6b28a65dc480
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2020-09-20 19:06:49 -0400

    updated: run-main-build-package.sh (execute 'R CMD check' on both with-vignettes and without-vignettes package tarballs)

--- 101-package-stcCropYield/run-main-build-package.sh
+++ 101-package-stcCropYield/run-main-build-package.sh
@@ -14,19 +14,21 @@ cp    $0         ${outputDIR}/code
 ########################################################
 ##### Assemble contents of R package
 packageName=stcCropYield
+
 myRscript=${codeDIR}/main-assemble-package.R
 stdoutFile=${outputDIR}/stdout.R.`basename ${myRscript} .R`
 stderrFile=${outputDIR}/stderr.R.`basename ${myRscript} .R`
 Rscript ${myRscript} ${codeDIR} ${outputDIR} ${packageName} > ${stdoutFile} 2> ${stderrFile}
 
-##### Build the R package (create *.tar.gz file)
-cd ${outputDIR}
-#R CMD build --md5 ${outputDIR}/${packageName} > ${outputDIR}/stdout.R.build 2> ${outputDIR}/stderr.R.build
-#R CMD build --md5 --no-build-vignettes ${outputDIR}/${packageName} > ${outputDIR}/stdout.R.build 2> ${outputDIR}/stderr.R.build
-
 ##### Check the newly built R package
 # remember: define the environment variable R_LIBS_USER
 # so this build script knows where the dependencies are,
 # so that the following command can work properly.
+
+cd ${outputDIR}/build-no-vignettes
+R CMD check ${packageName}_*.tar.gz > stdout.R.check 2> stderr.R.check
 # R CMD check --as-cran ${packageName}_*.tar.gz > ${outputDIR}/stdout.R.check 2> ${outputDIR}/stderr.R.check
-R CMD check ${packageName}_*.tar.gz > ${outputDIR}/stdout.R.check 2> ${outputDIR}/stderr.R.check
+
+cd ${outputDIR}/build-vignettes
+R CMD check ${packageName}_*.tar.gz > stdout.R.check 2> stderr.R.check
+# R CMD check --as-cran ${packageName}_*.tar.gz > ${outputDIR}/stdout.R.check 2> ${outputDIR}/stderr.R.check
