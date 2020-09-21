4786de074509fe062a06ff87ffbe993fe2eb1a57
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2020-09-20 22:20:02 -0400

    updated: run-main-build-package.bat

--- 101-package-stcCropYield/run-main-build-package.bat
+++ 101-package-stcCropYield/run-main-build-package.bat
@@ -40,7 +40,7 @@ Rscript %codeDIR%\main-assemble-package.R %codeDIR% %outROOT% %packageName% 1> %
 
 :: Build the R package (create *.tar.gz file)
 Z:
-cd %outROOT%\build-no-vignetttes
+cd %outROOT%\build-no-vignettes
 ::R CMD build --md5 --no-build-vignettes %outROOT%\%packageName% 1> %outROOT%\stdout.R.build 2> %outROOT%\stderr.R.build
 
 :: Check the newly built R package
